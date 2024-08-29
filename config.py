82e80ea7ae59e68d70aa556d5062432a3b5ae7f2
Raoul Biagioni <raoulbia@gmailcom>
2024-08-29 15:28:33 +0100

    updated historical context

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BASE_DIR = 'data/'
 TRAIN_FILE = os.path.join(BASE_DIR, 'train/train.csv')
 TEST_FILE = os.path.join(BASE_DIR, 'test/test.csv')
 TRAIN_OUTPUT = os.path.join(BASE_DIR, 'train/jsonl/claude_train_v2.jsonl')
-TEST_OUTPUT = os.path.join(BASE_DIR, 'test/jsonl/test.jsonl')
+TEST_OUTPUT = os.path.join(BASE_DIR, 'test/jsonl/claude_test_v2.jsonl')
 SUBMISSION_FILENAME =  os.path.join(BASE_DIR,"submissions/submission_claude_sonnet_v2.csv")
 
 # Data preparation parameters
@@ -16,11 +16,12 @@ FARE_LABELS = ['Low', 'Medium-Low', 'Medium-High', 'High']
 
 # Prompt generation
 HISTORICAL_CONTEXT = """
-The Titanic sank on its maiden voyage in 1912 after hitting an iceberg. The ship had a severe shortage of lifeboats, 
-with capacity for only about half of those onboard. First-class passengers, especially women and children, had a higher 
-survival rate due to their proximity to the boat deck and preferential treatment during evacuation. The 'women and children first' 
-protocol was generally followed, particularly in first and second class areas.
-"""
+Inconsistent Protocols: The "women and children first" protocol was applied inconsistently. First Officer William Murdoch 
+interpreted it to mean women and children first, while Second Officer Charles Lightoller adhered strictly to that, 
+leading to disparities in survival rates.
+Panic and Delays: Panic erupted among passengers, especially in third class, where many were unaware of the severity of 
+the situation and struggled to reach the lifeboats due to flooding and confusion.
+Musicians' Role: none survived."""
 
 # System message for AI
 SYSTEM_MESSAGE = """
